ba7abee6639c4019f4c83423eefc497d86cf4ae6
newco_rodick <newcompik1999@mail.ru>
2026-01-21 06:50:00 +0300

    created a endpoints for shop and seller

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,3 +1,120 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.shop.serializers import CategorySerializer, ProductSerializer
+from apps.shop.models import Category, Product
+from apps.sellers.models import Seller
+
+tags = ["Shop"]
+
+
+class CategoriesView(APIView):
+    serializer_class = CategorySerializer
+
+    @extend_schema(
+        summary="Categories Fetch",
+        description="""
+            This endpoint returns all categories.
+        """,
+        tags=tags
+    )
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        serializer = self.serializer_class(categories, many=True)
+        return Response(data=serializer.data, status=200)
+
+    @extend_schema(
+        summary="Category Creating",
+        description="""
+            This endpoint creates categories.
+        """,
+        tags=tags
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            new_cat = Category.objects.create(**serializer.validated_data)
+            serializer = self.serializer_class(new_cat)
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+class ProductsByCategoryView(APIView):
+    serializer_class = ProductSerializer
+
+    @extend_schema(
+        operation_id="category_products",
+        summary="Category Products Fetch",
+        description="""
+            This endpoint returns all products in a particular category.
+        """,
+        tags=tags
+    )
+    def get(self, request, *args, **kwargs):
+        category = Category.objects.get_or_none(slug=kwargs["slug"])
+        if not category:
+            return Response(data={"message": "Category does not exist!"}, status=404)
+        products = Product.objects.select_related("category", "seller", "seller__user").filter(category=category)
+        serializer = self.serializer_class(products, many=True)
+        return Response(data=serializer.data, status=200)
+
+
+class ProductsView(APIView):
+    serializer_class = ProductSerializer
+
+    @extend_schema(
+        operation_id="all_products",
+        summary="Product Fetch",
+        description="""
+            This endpoint returns all products.
+        """,
+        tags=tags
+    )
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.select_related("category", "seller", "seller__user").all()
+        serializer = self.serializer_class(products, many=True)
+        return Response(data=serializer.data, status=200)
+
+
+class ProductsBySellerView(APIView):
+    serializer_class = ProductSerializer
+
+    @extend_schema(
+        summary="Seller Products Fetch",
+        description="""
+            This endpoint returns all products in a particular seller.
+        """,
+        tags=tags
+    )
+    def get(self, request, *args, **kwargs):
+        seller = Seller.objects.get_or_none(slug=kwargs["slug"])
+        if not seller:
+            return Response(data={"message": "Seller does not exist!"}, status=404)
+        products = Product.objects.select_related("category", "seller", "seller__user").filter(seller=seller)
+        serializer = self.serializer_class(products, many=True)
+        return Response(data=serializer.data, status=200)
+
+
+class ProductView(APIView):
+    serializer_class = ProductSerializer
+
+    def get_object(self, slug):
+        product = Product.objects.get_or_none(slug=slug)
+        return product
+
+    @extend_schema(
+        operation_id="product_detail",
+        summary="Product Details Fetch",
+        description="""
+            This endpoint returns the details for a product via the slug.
+        """,
+        tags=tags
+    )
+    def get(self, request, *args, **kwargs):
+        product = self.get_object(kwargs['slug'])
+        if not product:
+            return Response(data={"message": "Product does not exist!"}, status=404)
+        serializer = self.serializer_class(product)
+        return Response(data=serializer.data, status=200)
